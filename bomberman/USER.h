8dd856a0ac6068d3fd84fc48925a583959bfa31e
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-07-04 02:07:49 +0800

    Added generic user data enums and structs.

--- bomberman/USER.h
+++ bomberman/USER.h
@@ -10,5 +10,28 @@ Information such as this should be passed to each respective level function.
 */
 #ifndef USER_H
 #define USER_H
+#include <stdint.h>
+
+#define USER_LOC "assets/user_black.png"
+#define USER_MOVEMENT_L0 56
+
+typedef enum collision {
+	OPEN = 0,
+	CLOSED = 1
+} collision;
+
+typedef struct USER_DAT {
+	uint8_t* imageData;
+	int user_pos_x_prev;
+	int user_pos_y_prev;
+	int user_pos_x;
+	int user_pos_y;
+	int user_score;
+	int user_timer;
+	collision collision_status_top;
+	collision collision_status_bottom;
+	collision collision_status_right;
+	collision collision_status_left;
+} USER_DAT;
 
 #endif // USER_H
